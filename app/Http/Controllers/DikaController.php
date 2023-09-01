064810cd9e4ba0154f87128fe8261583dd11177f
Putriiaks <biruprisma1@gmail.com>
2023-09-01 16:01:35 +0700

    update0109

---
+++ app/Http/Controllers/DikaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Dika;
+use Illuminate\Http\Request;
+
+class DikaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Dika $dika)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Dika $dika)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Dika $dika)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Dika $dika)
+    {
+        //
+    }
+}
--- database/migrations/2023_09_01_035759_create_dikas_table.php
+++ database/migrations/2023_09_01_035759_create_dikas_table.php
@@ -11,10 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('kategoris', function (Blueprint $table) {
+        Schema::create('dikas', function (Blueprint $table) {
             $table->id();
-            $table->string('nama');
-            $table->string('kategori');
+            $table->string('nama_dika');
             $table->timestamps();
         });
     }
@@ -24,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('kategoris');
+        Schema::dropIfExists('dikas');
     }
 };
